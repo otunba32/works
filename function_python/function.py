97dc6318e89838fa2193b0b4b8d3128865365715
codetrybe <tpauldike@yahoo.com>
2023-03-10 02:28:04 +0100

    added more codes

--- function_python/function.py
+++ function_python/function.py
@@ -2,6 +2,17 @@ def greet(name):
     '''
     greet salutes the name passed to it
     '''
-    print('Good day {}'.format(name))
+    print('Good day {}!'.format(name))
 
-greet('Maleek')
+def count_to(number):
+    '''
+    counts from 1 to the 'number' given
+    '''
+    number = int(number)
+    for i in range(1, number+1):
+        print(i, end=' ')
+    print()
+
+if __name__ == '__main__':
+    greet('Maleek')
+    count_to(10)
